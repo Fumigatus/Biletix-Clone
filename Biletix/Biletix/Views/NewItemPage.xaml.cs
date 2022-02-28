56b7c8ea8d4a489a0f6c80f023893e11752b46ba
Alpcan BELEN <alpcan237@gmail.com>
2022-03-01 00:33:18 +0300

    flyout bitti, sayfa yönlendirmesi yapılacak

---
+++ Biletix/Biletix/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Biletix.Models;
+using Biletix.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Biletix.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
